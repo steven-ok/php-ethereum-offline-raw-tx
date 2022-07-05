202fc71442b7b8fadd22048da0b07aabdb6b44ef
Steven <chunmeng.jiang@gmail.com>
2022-07-05 15:08:49 +0800

    getRaw 优化

--- src/Ethereum/Transaction.php
+++ src/Ethereum/Transaction.php
@@ -42,7 +42,7 @@ class Transaction {
         ];
     }
 
-    public function getRaw(string $privateKey, int $chainId = 0, $options = []): string {
+    public function getRaw(string $privateKey, int $chainId = 0, $options = []) {
         if ($chainId < 0) {
             throw new RuntimeException('ChainID must be positive');
         }
